d8c24aeca2016b81f375ef60cb6429be5ff5e581
Nikitoq <nikita.nikitov8546@gmail.com>
2026-04-23 19:33:21 +0200

    made hw1 and hw2

---
+++ hw2.py
@@ -0,0 +1,22 @@
+import re
+
+def generator_numbers(text: str):
+    pattern = r"[+-]?\d+(?:\.\d+)?"
+    numbers = [float(x) for x in re.findall(pattern, text)]
+    yield numbers
+
+
+
+
+def sum_profit(text: str, func):
+    counter = 0
+    for numbers in func(text):
+        for number in numbers:
+            counter += number
+    return counter
+
+
+
+text = "Загальний дохід працівника складається з декількох частин: 1000.01 як основний дохід, доповнений додатковими надходженнями 27.45 і 324.00 доларів."
+total_income = sum_profit(text, generator_numbers)
+print(f"Загальний дохід: {total_income}")
