a69d6f9bdc6d7ebdea96633cc92b9d6d209b827e
Nikitoq <nikita.nikitov8546@gmail.com>
2026-06-21 17:15:14 +0200

    hw3

--- hw3/hw3.py
+++ hw3/hw3.py
@@ -1,38 +1,44 @@
 import sys
-from collections import namedtuple, Counter
-
-file_path = sys.argv[1]
-level = sys.argv[2]
+from collections import Counter
 
 def parse_log_line(line: str) -> dict:
-    named_lines_dict = namedtuple("Log_infos", ["date", "time", "level", "message"])
     line = line.split()
     message = " ".join(line[3:])
-    line_dict = named_lines_dict(line[0], line[1], line[2], message)
+    line_dict = {"date": line[0], "time": line[1], "level": line[2], "message": message}
     return line_dict
 
-
 def load_logs(file_path: str) -> list:
+    logs = list()
     with open(file_path, "r", encoding="UTF-8") as file:
-        line = file.readline()
-    logs = parse_log_line(line)
+        for line in file:
+            logs.append(parse_log_line(line))
     return logs
 
 def filter_logs_by_level(logs: list, level:str) -> list:
-    print(
-        f"""Рівень логування | Кількість\n
-        INFO info\n
-        DEBUG debug\n
-        ERROR error\n
-        WARNING warning\n
-        """
-            )
+    return list(filter(lambda log: log["level"] == level.upper(), logs))
 
 def count_logs_by_level(logs: list) -> dict:
-    counts = Counter(load_logs(logs))
-
+    return dict(Counter(log["level"] for log in logs))
 
 def display_log_counts(counts: dict):
-    pass
+    print("Рівень логування | Кількість")
+    print("-----------------|----------")
+    for level, count in counts.items():
+        print(f"{level:<16} | {count}")
 
-print(count_logs_by_level(file_path))
+if __name__ == "__main__":
+    file_path = sys.argv[1]
+
+    logs = load_logs(file_path)
+
+    counts = count_logs_by_level(logs)
+    display_log_counts(counts)
+
+    if len(sys.argv) == 3:
+        level = sys.argv[2].upper()
+
+        filtered = filter_logs_by_level(logs, level)
+
+        print(f"\nДеталі логів для рівня '{level}':")
+        for log in filtered:
+            print(f"{log['date']} {log['time']} - {log['message']}")
